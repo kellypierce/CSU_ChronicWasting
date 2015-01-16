f76c8655a6bfccad194712b195454b7af119eeab
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-01-16 12:36:13 -0700

    added plot output as .png

--- Scripts/Seq_Coverage.r
+++ Scripts/Seq_Coverage.r
@@ -10,8 +10,9 @@ phi=seq(0,0.01,by=0.001)
 nr=n*d*(x/phi)/fl
 nl=nr/200000000
 par(mar=c(5,5,5,2))
+png(file='~/Dropbox/ColoState//Projects/Outputs/LanesNeeded.png', height=10, width=10,
+    res=300, unit='cm')
 plot(phi, nl, xlab="Per Site SNP Probability",  
      ylab="Lanes of Illumina HiSeq\n(est 200 million reads/lane)",
      main="30X Coverage Depth/20,000 SNPs/250 Samples", las=1)
-  
-
+dev.off()
