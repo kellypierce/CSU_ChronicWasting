3f5062c540f172ac4d438a386828c495ad6ddbd0
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-01-16 15:50:22 -0700

    fixed plot margins

--- Scripts/Seq_Coverage.r
+++ Scripts/Seq_Coverage.r
@@ -9,10 +9,11 @@ phi=seq(0,0.01,by=0.001)
 
 nr=n*d*(x/phi)/fl
 nl=nr/200000000
-par(mar=c(5,5,5,2))
+
 png(file='~/Dropbox/ColoState//Projects/Outputs/LanesNeeded.png', height=10, width=10,
     res=300, unit='cm')
+par(mar=c(5,6,6,2))
 plot(phi, nl, xlab="Per Site SNP Probability",  
      ylab="Lanes of Illumina HiSeq\n(est 200 million reads/lane)",
-     main="30X Coverage Depth/20,000 SNPs/250 Samples", las=1)
+     main="20,000 SNPs\n30X Coverage Depth\n250 Samples", las=1)
 dev.off()
