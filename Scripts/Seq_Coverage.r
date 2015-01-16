7580c67be4316238d234d211428872dd0078a4f8
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-01-16 12:08:44 -0700

    first commit

---
+++ Scripts/Seq_Coverage.r
@@ -0,0 +1,17 @@
+#Number of reads required for different estimates of phi
+
+d=30 #coverage depth
+fl=300 #average fragment length
+x=20000 #desired number of SNPs
+n=250 #number of samples
+
+phi=seq(0,0.01,by=0.001)
+
+nr=n*d*(x/phi)/fl
+nl=nr/200000000
+par(mar=c(5,5,5,2))
+plot(phi, nl, xlab="Per Site SNP Probability",  
+     ylab="Lanes of Illumina HiSeq\n(est 200 million reads/lane)",
+     main="30X Coverage Depth/20,000 SNPs/250 Samples", las=1)
+  
+
