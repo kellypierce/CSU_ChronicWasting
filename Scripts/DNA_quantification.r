41a14ccd66e361cf746a5f8ae46ab95dd7a29108
kellyp2738 <kellyannepierc@gmail.com>
2015-09-16 13:37:20 -0600

    Added buffy coat extraction quantification.

--- Scripts/DNA_quantification.r
+++ Scripts/DNA_quantification.r
@@ -20,3 +20,16 @@ exhaust.sample<-dna.quant[which(dna.quant$Approx..ng.DNA.avail..For.final.digest
 head(exhaust.sample)
 exhaust.DNA.A<-exhaust.sample[which(exhaust.sample$DNA.A.or.DNA.B.== 'A'),]
 exhaust.DNA.A
+
+# Buffy coat extraction summary statistics
+
+# lazy entry of recovery stats (no excel sheet yet)
+twenty<-c(850, 90.5, 1279.2, 1606.8, 1021.8, 35.1, 579.15, 573.5, 873.6, 657.15, 430.95)
+forty<-c(1131, 795.6, 1154.4, 1053, 2277.6, 943.8, 1567.8, 626.3, 842.4, 343.2, 1583.4)
+
+png(file='~/Dropbox/ColoState/Projects/Outputs/BuffyCoatExt.png', height=10, width=12, units='cm', res=300)
+par(mfrow=c(1,1), mar=c(4,5,2,2))
+boxplot(recovery, las=1, ylab='', main='Buffy Coat Extractions', 
+        names=c(expression(paste('20', mu, 'l')), expression(paste('40', mu, 'l'))))
+mtext(side=2, text='Mass DNA (ng)', line=4)
+dev.off()
