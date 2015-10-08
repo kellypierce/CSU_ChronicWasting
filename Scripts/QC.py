33a1044ee005716033ebe1396ac2d56384bec55d
kellyp2738 <kellyannepierc@gmail.com>
2015-10-08 17:14:58 -0600

    Final version.

--- Scripts/QC.py
+++ Scripts/QC.py
@@ -6,26 +6,55 @@ import subprocess
 
 # run QC on the library-demultiplexed ddRADseq data with DBRs:
 
-#m.group(1) for l in lines for m in [regex.search(l)] if m
-
 # check for barcodes, read 1
 def barcode_r1(directory, out_name):
 	files = os.listdir(directory)
-	print files
-	#r1_files = []
+	out_dir = directory + '/barcodes/'
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
 	for f in files:
 		r1 = re.findall(r'R1', f)
 		if r1:
-			#r1_files.append(f)
-			barcode_check_call = 'zcat ' + directory + f + " | sed -n '2~4p' | cut -c 1-5 | sort | uniq -c | sort -nr -k 1 > " + directory + f + '_' + out_name
+			barcode_check_call = 'zcat ' + directory + f + " | sed -n '2~4p' | cut -c 1-5 | sort | uniq -c | sort -nr -k 1 > " + out_dir + f + '_' + out_name
 			subprocess.call(barcode_check_call, shell=True)
 
 # check for cutsite, read 1
-#cutsite_check_call = 'zcat ' + library_name + " | sed -n '2~4p' | cut -c 6-10 | sort | uniq -c | sort -nr -k 1 > " + out_name
-#subprocess.call(cutsite_call, shell=True)
+def cutsite_r1(directory, out_name):
+	files = os.listdir(directory)
+	out_dir = directory + '/cutsites/'
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
+	for f in files:
+		r1 = re.findall(r'R1', f)
+		if r1:
+			cutsite_check_call = 'zcat ' + directory + f + " | sed -n '2~4p' | cut -c 6-10 | sort | uniq -c | sort -nr -k 1 > " + out_dir + f + '_' + out_name
+			subprocess.call(cutsite_check_call, shell=True)
+
+def cutsite_r2(directory, out_name):
+	files = os.listdir(directory)
+	out_dir = directory + '/cutsites_read2/'
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
+	for f in files:
+		r1 = re.findall(r'R2', f)
+		if r1:
+			cutsite_check_call = 'zcat ' + directory + f + " | sed -n '2~4p' | cut -c 9-14 | sort | uniq -c | sort -nr -k 1 > " + out_dir + f + '_' + out_name
+			subprocess.call(cutsite_check_call, shell=True)
 
 # check for degeneracy, read 2
-#degeneracy_check_call = 'zcat ' + library_name + " | sed -n 2~4p | cut -c 1-8 | sort | uniq -c | sort -nr -k 1 > " + out_name
-#subprocess.call(degeneracy_call, shell=True)
+def degeneracy_r2(directory, out_name):
+	files = os.listdir(directory)
+	out_dir = directory + '/degeneracy'
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
+	for f in files:
+		r1 = re.findall(r'R2', f)
+		if r1: 
+			degeneracy_check_call = 'zcat ' + directory + f + " | sed -n 2~4p | cut -c 1-8 | sort | uniq -c | sort -nr -k 1 > " + out_dir + f + '_' + out_name
+			subprocess.call(degeneracy_check_call, shell=True)
 
-barcode_r1('/home/antolinlab/Downloads/CWD_RADseq/', 'barcode_check')
+#barcode_r1('/home/antolinlab/Downloads/CWD_RADseq/', 'barcode_check')
+#cutsite_r1('/home/antolinlab/Downloads/CWD_RADseq/', 'cutsite_check')
+cutsite_r2('/home/antolinlab/Downloads/CWD_RADseq/', 'cutsite_r2_check')
+#degeneracy_r2('/home/antolinlab/Downloads/CWD_RADseq/', 'degeneracy_check')
+
