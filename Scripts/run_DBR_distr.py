6321749362350a953fa6cea3baf6dfddb1e6d34f
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-10-09 10:14:20 -0600

    Function calls to quality filter and count DBRs.

--- Scripts/run_DBR_distr.py
+++ Scripts/run_DBR_distr.py
@@ -1,24 +1,11 @@
 #!/usr/bin/PYTHON
 
-print 'running quality filtering and DBR counting for DBR distribution generation'
-
 import integrated_denovo_pipeline as pipeline
 import QC as qc
 import os
 import re
 
-def iterative_FASTQC(directory, out_name, q, p):
-    files = os.listdir(directory)
-    out_dir = directory + '/qual_filtered_R2_for_DBR_distr/'
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    print out_dir
-    for f in files:
-        r2 = re.findall(r'R2', f)
-        if r2: 
-            in_file = directory + f
-            out_file = out_dir + f + out_name
-            pipeline.FASTQ_quality_filter(in_file, out_file, q, p)
-
-iterative_FASTQC('/home/antolinlab/Downloads/CWD_RADseq/', 'qual_filtered_30.fastq.gz', 30, 50)
-qc.degeneracy_r2('/home/antolinlab/Downloads/CWD_RADseq/', 'qual_filtered_30_degeneracy_check')
+# run the FASTX toolkit quality filters on Read 2 only (lots of data; will be slow)
+# get the degeneracy stats for the quality filtered Read 2 files
+pipeline.iterative_FASTQ_quality_filter(directory = '~/CWD_RADseq/', out_dir = '/qual_filtered_R2_for_DBR_distr/', out_name = 'qual_filtered_30.fastq.gz', q = 30, p = 50, read = 'R2')
+qc.degeneracy_r2(directory = '~/CWD_RADseq/', out_name = 'qual_filtered_30_degeneracy_check')
