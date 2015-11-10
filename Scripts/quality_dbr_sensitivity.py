60a222d7f27d88498c2401ea47224a73e646fd35
kellyp2738 <kellyannepierc@gmail.com>
2015-11-10 12:47:44 -0700

    Minor edits

--- Scripts/quality_dbr_sensitivity.py
+++ Scripts/quality_dbr_sensitivity.py
@@ -19,7 +19,7 @@ def dbr_qual_sens(infile, outdir, outprefix, qual):
     if not os.path.exists(outdir):
             os.makedirs(outdir)
     
-    while qual <= 40:
+    while qual < 40:
     
         print ['Filtering at median quality = ' + str(qual)]
     
@@ -30,7 +30,7 @@ def dbr_qual_sens(infile, outdir, outprefix, qual):
     
         # Generate DBR distribution
         count_out = str(qual) + "median_quality_degeneracy_count.txt"
-        count_call = "zcat " + out_file + " | sed -n '2~4p' | cut -c 1-8 | sort | uniq -c | sort -nr -k 1 > " + outdir + '/' + count_out
+        count_call = "zcat " + out_file + " | sed -n '2~4p' | cut -c 2-9 | sort | uniq -c | sort -nr -k 1 > " + outdir + '/' + count_out
         subprocess.call(count_call, shell=True)
     
         print filter_call
@@ -39,9 +39,14 @@ def dbr_qual_sens(infile, outdir, outprefix, qual):
         # Recursively call function so that the filtering happens on iteratively smaller input datasets (hopefully to increase speed and avoid recalculation median quality multiple times for low quality sequences
         return dbr_qual_sens(out_file, outdir, outprefix, qual+5)
 
-out_dir = "/home/antolinlab/Desktop/CSU_ChronicWasting/PilotAnalysis/QualDBR_Sensitivity"
-original_data = "~/Downloads/Sample1_ACTTGA_L008_R1_001.fastq.gz"
-out_prefix = "Sample1_ACTTGA_L008_R1_001_filtered_temp"
-dbr_qual_sens(original_data, out_dir, out_prefix, 20)
+#out_dir = "/home/antolinlab/Desktop/CSU_ChronicWasting/PilotAnalysis/QualDBR_Sensitivity"
+#original_data = "~/Downloads/Sample1_ACTTGA_L008_R1_001.fastq.gz"
+#out_prefix = "Sample1_ACTTGA_L008_R1_001_filtered_temp"
+
+out_dir = "/home/antolinlab/Downloads/CWD_RADseq/Library12_DBR_distr/"
+original_data = "/home/antolinlab/Downloads/CWD_RADseq/Library12_S65_L008_R2_001.fastq.gz"
+out_prefix = "Library12_S65_L008_R2_001_dbr_distr"
+
+dbr_qual_sens(original_data, out_dir, out_prefix, 35)
 
 
