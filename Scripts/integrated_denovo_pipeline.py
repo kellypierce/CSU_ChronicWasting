32d361142bdba09657854487a66be93fd2f7d4fe
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-02-19 16:10:54 +1030

    Debugging v9

--- Scripts/integrated_denovo_pipeline.py
+++ Scripts/integrated_denovo_pipeline.py
@@ -384,8 +384,6 @@ def denovo_Stacks(in_dir, denovo_path, stacks_executables, out_dir, m, n, b, D):
             rm_unmatched = True
         else:
             
-            # full path to trimmed file
-            new_path=os.path.join(in_dir, i)            
             # Run ustacks
             # example usage: ustacks -t fastq -f ./samples/f0_male.fq    -o ./stacks -i 1 -d -r -m 3 -p 15
             ustacks_args = [ustacks_path, ' -t fastq ' + ' -f ' + new_path + ' -o ', out_dir, ' -m ', str(m), ' -r -R']
@@ -403,6 +401,9 @@ def denovo_Stacks(in_dir, denovo_path, stacks_executables, out_dir, m, n, b, D):
         if 'tags' in j: # we only want the tags.tsv files for ccstacks
             
             print cstacksMessageTemplate.substitute(name = j)
+            
+            # full path to trimmed file
+            new_path=os.path.join(in_dir, j)            
             
             # generate a string for the -s cstacks argument
             s_list.append('-s ')
