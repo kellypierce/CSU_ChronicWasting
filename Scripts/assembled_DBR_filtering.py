573d7fae7bf76832e02633def32fbae26ccfd817
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-03-28 16:12:49 +1030

    Added accidentally removed function argument.

--- Scripts/assembled_DBR_filtering.py
+++ Scripts/assembled_DBR_filtering.py
@@ -277,7 +277,7 @@ class filterGroup():
                  n_expected, # the number of differences to be tolerated
                  dict_file, # a single dictionary of DBRs (for one library only) 
                  sampleID,
-                 libraryID,
+                 logfile,
                  test_dict=True, # optionally print testing info to stdout for checking the dictionary construction
                  phred_dict=phred_dict, # dictionary containing ASCII quality filter scores to help with tie breaks
                  samMapLen=None):
@@ -287,7 +287,7 @@ class filterGroup():
         self.n_expected = n_expected
         self.dict_file = dict_file
         self.sampleID = sampleID
-        self.libraryID = libraryID
+        self.logfile = logfile
         self.test_dict = test_dict
         self.phred_dict = phred_dict
         self.samMapLen = samMapLen
