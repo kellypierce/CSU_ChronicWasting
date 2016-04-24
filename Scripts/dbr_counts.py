890a7fe96ee4f6d88176391dbdcc3fff79bb5691
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-04-24 20:14:20 +0930

    Fixed dbr start and stop values.

--- Scripts/dbr_counts.py
+++ Scripts/dbr_counts.py
@@ -8,9 +8,9 @@ from subprocess import call, Popen, PIPE
 import subprocess
 from assembled_DBR_filtering import *
 
-parallel_DBR_count(in_dir = '/home/pierce/CWD_RADseq/raw_new/qual_filtered',
-                   dbr_start = -8,
-                   dbr_stop = None,
+parallel_DBR_count(in_dir = '/home/pierce/CWD_RADseq/raw_new/qual_filtered/',
+                   dbr_start = -9,
+                   dbr_stop = -2,
                    save = '/home/pierce/CWD_RADseq/raw_new/degeneracy_checks/',
                    saveType = 'text')
 
